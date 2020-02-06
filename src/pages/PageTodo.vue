219fdb9a1aa74382ed39e93d182b97d49c469e01
Andrea Reber <reberan@andreas-imac.home>
2020-02-06 15:52:52 +0100

    Section: 5, Items: 30, 31, 32

---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page padding>
+    <p>Todo Page</p>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,11 @@
 const routes = [
   {
     path: "/",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }]
+    component: () => import("layouts/Layout.vue"),
+    children: [
+      { path: "", component: () => import("pages/PageTodo.vue") },
+      { path: "/settings", component: () => import("pages/PageSettings.vue") }
+    ]
   }
 ];
 
